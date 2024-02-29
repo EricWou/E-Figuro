af1db261786db67f239a57eca15ea98c374fca80
EricWou <146874839+EricWou@users.noreply.github.com>
2024-02-29 13:28:54 -0500

    1st version of project

---
+++ E-Figuro_v1/PageFeatures.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace E_Figuro
+{
+    /// <summary>
+    /// Interaction logic for PageFeatures.xaml
+    /// </summary>
+    public partial class EmployeeFeaturesPage : Window
+    {
+        public EmployeeFeaturesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registration_button_Click(object sender, RoutedEventArgs e)
+        {
+            Registration window1 = new Registration();
+            window1.Show();
+            this.Close();
+        }
+
+        private void logout_button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window2 = new MainWindow();
+            window2.Show();
+            this.Close();
+        }
+    }
+}
